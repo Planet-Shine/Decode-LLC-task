84807f80220f7c6210293e4dd79ea899119d655f
PavelOfficial <pavelofficial@ya.ru>
2016-11-16 21:00:42 +0300

    project upload

--- app/Todo/Todo.controller.js
+++ app/Todo/Todo.controller.js
@@ -1,11 +1,113 @@
-const applicationModule = require('../todo.js');
+export default function (applicationModule) {
+    var TodoCtrl = ['$scope', '$accountResource', '$projectResource', '$cookies', '$mdSidenav', function ($scope, $accountResource, $projectResource, $cookies, $mdSidenav) {
+        var self               = this,
+            session            = $cookies.get('session'),
+            sessionGetTries    = 0,
+            sessionGetMax      = 5,
+            loaderModificators = {
+                'showed' : 'page-cover_showed',
+                'hidden' : 'page-cover_hidden',
+                'hiding' : ''
+            },
+            loaderTimeout = 1000;
 
-var TodoCtrl = [function () {
-
-}];
-
-applicationModule.controller('TodoCtrl', TodoCtrl);
+        $scope.account  = {};
 
 
+        this.searchText = '';
+        this.selectedProjectIndex = 0;
+        this.searchInputHidden = true;
+        this.projects = [];
+        this.loaderModificator = loaderModificators['showed'];
+        this.selectProject = function (index) {
+            self.selectedProjectIndex = index;
+        };
+
+        if (session) {
+            checkSession();
+        } else {
+            getSession();
+        }
+
+        function checkSession (callback) {
+            $accountResource.request('session', {'session' : session})
+                .get()
+                .$promise.then(function (data) {
+                    loadAppData();
+                }, function (error) {
+                    getSession(callback);
+                    // $accountResource.handleError(error);
+                });
+        }
+
+        function loadAppData () {
+            var requestCount  = 0,
+                requestTarget = 2;
+
+            $accountResource.request('account', {'session' : session})
+                .get()
+                .$promise.then(function (data) {
+                    $scope.account = data.Account;
+                    requestCount += 1;
+                    next();
+                }, $accountResource.handleError);
+
+            $projectResource.request('fetch', {'session' : session})
+                .get()
+                .$promise.then(function (data) {
+                    self.projects = data.projects;
+                    requestCount += 1;
+                    next();
+                }, $projectResource.handleError);
+
+            function next () {
+                if (requestCount === requestTarget) {
+                    self.initializeGui();
+                }
+            }
+
+        }
+
+        function getSession (callback) {
+            $accountResource.request('signup', null)
+                .save()
+                .$promise.then(function (data) {
+                    if ($accountResource.isError(data)) {
+                        $accountResource.handleError(data);
+                    } else {
+                        session = data.session;
+                        $cookies.put('session', session);
+                        checkSession(callback);
+                    }
+                }, function (error) {
+                    sessionGetTries += 1;
+                    if (sessionGetTries < sessionGetMax) {
+                        getSession(callback);
+                    } else {
+                        $accountResource.handleError(error);
+                    }
+                });
+        }
+
+        this.initializeGui = function () {
+            this.loaderModificator = loaderModificators['hiding'];
+            setTimeout(function () {
+                self.loaderModificator = loaderModificators['hidden'];
+                $scope.$apply();
+            }, loaderTimeout);
+        };
+
+        this.openSidePanel = function () {
+            $mdSidenav('side-panel')
+                .toggle()
+                .then(function () {
+                    console.log('ready!');
+                });
+        };
+
+    }],
+        ctrl     = applicationModule.controller('TodoCtrl', TodoCtrl);
 
 
+    require('./profileInfo.directive.js')(ctrl);
+};
--- app/todo.js
+++ app/todo.js
@@ -1,6 +1,21 @@
 require('angular-material');
+require('angular-resource');
+
 const angular = require('angular');
+const applicationModule = angular.module('todo', [
+    'ngMaterial',
+    'ngResource',
+    'ngCookies'
+]);
 
-const applicationModule = angular.module('todo', ['ngMaterial']);
+require('./layout-fixes');
 
-module.exports = applicationModule;
+// Сервисы.
+require('./resource/account')(applicationModule);
+require('./resource/project')(applicationModule);
+require('./resource/task')(applicationModule);
+
+require("./styl/index.less"); // Собираем стили.
+
+require('./todo.config')(applicationModule);
+require('./todo/todo.controller')(applicationModule);
