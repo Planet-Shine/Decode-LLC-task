92d2dde404083973bd2c6756f5d5449b8892f0a3
PavelOfficial <pavelofficial@ya.ru>
2016-11-20 00:53:17 +0300

    layout fixes

---
+++ app/layout-fixes.js
@@ -0,0 +1,18 @@
+var $ = require('jquery');
+
+$(function () {
+    var doc                 = $(document),
+        workspaceBoard      = $(".workspace-board"),
+        addTodoButton       = $('.add-todo-button'),
+        startBottomPosition = 30;
+
+    function setNewPositionOfAddButton () {
+        var scrollTop = workspaceBoard.scrollTop();
+        addTodoButton.css('bottom', (startBottomPosition - scrollTop) + 'px');
+    }
+
+    workspaceBoard.scroll(function () {
+        setNewPositionOfAddButton();
+    });
+    setNewPositionOfAddButton();
+});
